6951cc173e9c05f01e888ce0dae869323966e055
Sonali Shakya <sonalishakya@Admins-MacBook-Air.local>
2024-11-26 10:46:45 +0530

    Remove Item

--- src/main/java/com/ondc/yugabyte_integration/Repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ondc.yugabyte_integration.Repository;
-
-import com.ondc.yugabyte_integration.Entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
